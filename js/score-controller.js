1e4c41aca155328c860a090cf3abe60b888c6514
Uddeshya <uddeshya_kumar@ymail.com>
2022-10-25 22:00:39 +0530

    small fix

--- js/score-controller.js
+++ js/score-controller.js
@@ -13,7 +13,7 @@ function Score() {
     };
     this.setHighscore = () => window.localStorage.setItem('highscore', this.score);
     this.initializeScore = () => {
-        this.highScore = parseInt(window.localStorage.getItem('highscore')) ?? 0;
+        this.highScore = parseInt(window.localStorage.getItem('highscore') ?? 0);
         this.score = 0;
         scoreDom.innerText = `0 / ${this.highScore}`;
     }
