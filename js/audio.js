5a8fbb9d61ef7e7ec25c2ecc5b5168e856849012
Uddeshya <uddeshya_kumar@ymail.com>
2022-10-25 22:08:40 +0530

    audio path update

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,8 @@
-export const startAudio = new Audio('../audio/game_start.mp3');
-export const endAudio = new Audio('../audio/game_over.mp3');
-export const pauseAudio = new Audio('../audio/game_pause.mp3');
-export const highscoreAudio = new Audio('../audio/highscore.mp3');
+const basePath = location.host === 'drabzee.github.io'
+                  ? 'https://raw.githubusercontent.com/drabzee/snake-game/main'
+                  : '..'
+
+export const startAudio = new Audio(basePath + '/audio/game_start.mp3');
+export const endAudio = new Audio(basePath + '/audio/game_over.mp3');
+export const pauseAudio = new Audio(basePath + '/audio/game_pause.mp3');
+export const highscoreAudio = new Audio(basePath + '/audio/highscore.mp3');
